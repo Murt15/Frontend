2dc244a2384a2b60da8a748c234facaf69c29e92
Murt15 <murtazat941@gmail.com>
2023-01-07 19:02:28 +0530

    Starting with react

---
+++ Starting with react/src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense item</h2>
+      <p>Food Rs 10</p>
+      <p>Petrol Rs 100</p>
+      <p>Movies rs 100</p>
+    </div>
+  );
+}
+
+export default ExpenseItem;
