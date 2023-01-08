f90a648afcfa073ca15528831ab247868348e6de
Murt15 <murtazat941@gmail.com>
2023-01-08 12:37:07 +0530

    Adding props

--- Starting with react/src/components/ExpenseItem.js
+++ Starting with react/src/components/ExpenseItem.js
@@ -1,17 +1,13 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 10, 15);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 295.99;
-  const location = "Mumbai";
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <div>{location}</div>
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <div>{props.location}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
