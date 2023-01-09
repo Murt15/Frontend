f1aac2fcc586455f502752c371586f7a60695eff
Murt15 <murtazat941@gmail.com>
2023-01-09 09:54:57 +0530

    Adding Change Expense Button

--- Starting with react/src/components/Expenses/ExpenseItem.js
+++ Starting with react/src/components/Expenses/ExpenseItem.js
@@ -1,21 +1,31 @@
+import React, { useState } from "react";
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
 const ExpenseItem = (props) => {
   const deleteHandler = (event) => {
-    //console.log(event.target.parentElement.innerHTML);
     event.target.parentElement.remove();
+  };
+  const [title, setTitle] = useState(props.title);
+  const clickHandler = () => {
+    setTitle("Updated");
+  };
+  const [expense, updatedExpense] = useState(props.amount);
+  const expenseHandler = () => {
+    updatedExpense(100);
   };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <ExpenseDetails
-        title={props.title}
+        title={title}
         location={props.location}
-        amount={props.amount}
+        amount={expense}
       ></ExpenseDetails>
-      <button onClick={deleteHandler}>Delete</button>
+      <button onClick={clickHandler}>Change title</button>
+      <button onClick={expenseHandler}>Change Expense</button>
     </Card>
   );
 };
