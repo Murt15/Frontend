463f47696fa30d6629adfec650c4c0a130c5664d
Murt15 <murtazat941@gmail.com>
2023-01-11 14:20:01 +0530

    CHNGES

--- Starting with react/src/components/Expenses/ExpenseItem.js
+++ Starting with react/src/components/Expenses/ExpenseItem.js
@@ -5,22 +5,10 @@ import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
 const ExpenseItem = (props) => {
-  const deleteHandler = (event) => {
-    event.target.parentElement.remove();
-  };
-  const [title, setTitle] = useState(props.title);
-  const clickHandler = () => {
-    setTitle("Updated");
-  };
-  const [expense, updatedExpense] = useState(props.amount);
-  const expenseHandler = () => {
-    updatedExpense(100);
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <ExpenseDetails title={title} amount={expense}></ExpenseDetails>
-      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 };
