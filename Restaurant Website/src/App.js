e3752677ce502ea4856381ba42ae4f6b6e529d39
Murt15 <murtazat941@gmail.com>
2023-01-24 13:17:44 +0530

    Adding Meal Summary

--- Restaurant Website/src/App.js
+++ Restaurant Website/src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Layout/Header";
+import MealSummary from "./components/Meals/MealSummary";
 
 function App() {
   return (
     <>
       <Header />
+      <MealSummary />
     </>
   );
 }
