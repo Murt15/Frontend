f99b551fd4aff9824ea4dbe18a37f144978e539d
Murt15 <murtazat941@gmail.com>
2023-01-24 13:51:57 +0530

    Adding Dummy Meals

--- Restaurant Website/src/App.js
+++ Restaurant Website/src/App.js
@@ -1,11 +1,12 @@
 import Header from "./components/Layout/Header";
-import MealSummary from "./components/Meals/MealSummary";
-
+import Meals from "./components/Meals/Meals";
 function App() {
   return (
     <>
       <Header />
-      <MealSummary />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
---
+++ Restaurant Website/src/components/Meals/Meals.js
@@ -0,0 +1,11 @@
+import MealSummary from "./MealSummary";
+import AvailaibleMeals from "./AvailaibleMeals";
+const Meals = () => {
+  return (
+    <>
+      <MealSummary />
+      <AvailaibleMeals />
+    </>
+  );
+};
+export default Meals;
