c3b59551083c824df98a65b08e73229c2dfb11b8
Murt15 <murtazat941@gmail.com>
2023-01-31 12:21:35 +0530

    Adding Loading State

--- Movies Project/src/App.js
+++ Movies Project/src/App.js
@@ -5,7 +5,9 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const fetchMoviesHandler = async () => {
+    setIsLoading(true);
     try {
       const response = await fetch("https://swapi.py4e.com/api/films/");
       const data = await response.json();
@@ -18,6 +20,7 @@ function App() {
         };
       });
       setMovies(transformedMovies);
+      setIsLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -28,7 +31,8 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && <MoviesList movies={movies} />}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
