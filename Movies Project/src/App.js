df2792acac780f5a3601c74041007ebd7e49005e
Murt15 <murtazat941@gmail.com>
2023-01-31 15:58:34 +0530

    Fetch Movie on Page load

--- Movies Project/src/App.js
+++ Movies Project/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
@@ -7,10 +7,11 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const fetchMoviesHandler = async () => {
+
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     try {
-      const response = await fetch("https://swapi.py4e.com/api/flms/");
+      const response = await fetch("https://swapi.py4e.com/api/films/");
       if (!response.ok) {
         throw new Error("Something went wrong ....Retrying");
       }
@@ -33,7 +34,10 @@ function App() {
       // }, 5000);
     }
     setIsLoading(false);
-  };
+  }, []);
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
   return (
     <React.Fragment>
       <section>
